38bc8c91f1a1744975426a5d0fa04912e954e2ae
RoyL21 <35479347+RoyL21@users.noreply.github.com>
2023-11-02 22:07:06 +0800

    Create receipt.php

---
+++ receipt.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pizzalia</title>
+    <link rel="stylesheet" href="style-checkout.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <header class="header">
+        <a href="#" class="Logo">Pizza<span>lia</span></a>
+        <nav class="navbar">
+            <a href="index.php">Home</a>
+            <a href="menu.php">Menu</a>
+            <a href="contact.php">Contact</a>
+            <a href="feedback.php">Feedback</a>
+        </nav>
+        <button class="btn">Login</button>
+    </header>
+
+    <div class="main-container">
+        <section class="Dishes-section">
+
+            <div class="dishGallery">
+                <div class="food-category">
+                    <h2>Receipt</h2>
+
+
+                </div>
+            </div>
+
+
+        </section>
+        <div class="cart-container" id="cartContainer">
+            <h2>Order Details</h2>
+            <div id="checkoutCartContent"></div>
+            <div id="checkoutCartTotal"></div>
+        </div>
+
+    </div>
+
+
+
+    <footer>
+        <div class="innerItem">
+            <h2>©<span style="color: #bb7b05">Pizza</span><span style="color: #ad2525">lia</span> 2023</h2>
+            <div class="company-address">
+                83 Pizza Street 111883 #01-22 <br>
+                See On Google Maps <br>
+                Opening Hours 9 A.M -9 P.M
+            </div>
+        </div>
+    </footer>
+
+    <script src="checkout.js"></script>
+</body>
+
+</html>
